3698c3496eca56b39eea23f2af4c7a8ffa958d0a
Matthew Tran <matt.h.tran@gmail.com>
2023-12-05 14:40:46 -0500

    complete lab

--- server/app.py
+++ server/app.py
@@ -21,7 +21,36 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def find_earthquake_by_id(id):
+    quake = Earthquake.query.filter_by(id=id).first()
+    response_body = {"message":f"Earthquake {id} not found."}
+    response_code = 404
+    if quake: 
+        response_body = {
+            "id": id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        }
+        response_code = 200
+    return make_response(response_body, response_code)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def find_earthquake_magnitude(magnitude):
+    quakes = []
+    for quake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        quakes.append({
+            "id": quake.id,
+            "location": quake.location,
+            "magnitude": quake.magnitude,
+            "year": quake.year
+        })
+    body = {
+        "count": len(quakes),
+        "quakes": quakes
+    }
+    return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
